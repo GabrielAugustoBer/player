2f5781f28c8499e9f238bdd2db315e0ad7cee5c6
Gabriel Augusto <92814110+GabrielAugustoBer@users.noreply.github.com>
2024-06-21 11:50:16 -0300

    config vercel

---
+++ src/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Defina o link do áudio diretamente aqui
+    var audioUrl = "https://9462.brasilstream.com.br/mp3";
+    
+    // Cria um objeto de áudio
+    var audioPlayer = new Audio(audioUrl);
+    
+    // Adiciona evento ao botão de reprodução
+    var playButton = document.getElementById('playButton');
+    playButton.addEventListener('click', function() {
+        audioPlayer.play();
+    });
+    
+    // Adiciona evento ao botão de pausa
+    var pauseButton = document.getElementById('pauseButton');
+    pauseButton.addEventListener('click', function() {
+        audioPlayer.pause();
+    });
+});
